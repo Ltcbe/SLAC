b9b0489af0f90380bbb01f4486547999a3de1f1b
Ltcbe <support@terminalcommun.be>
2025-10-26 01:29:22 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,2 +1,11 @@
+import Header from './components/Header'
 import Home from './pages/Home'
-export default function App(){ return <Home/> }
+
+export default function App() {
+  return (
+    <div className="min-h-full">
+      <Header />
+      <Home />
+    </div>
+  )
+}
